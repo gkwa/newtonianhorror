04625426c19c7d3089882e9d9f721affe8103fc9
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-06 09:53:07 -0700

    Add permalink

---
+++ templates/jovialgazelle/stuff.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+cat >{{ .Container }}.yml <<'EOF'
+#cloud-config
+package_update: true
+package_reboot_if_required: true
+
+packages:
+- unzip
+- curl
+
+write_files:
+
+- content: |
+    #!/usr/bin/env bash
+
+    set -e
+    set -u
+
+    cd /tmp
+
+    curl -Lo ringgem.zip https://github.com/taylormonacelli/ringgem/archive/refs/heads/master.zip
+    unzip ringgem.zip
+
+    # FIXME:
+    export HOME=/root
+
+    task --dir=ringgem-master install-nix-on-linux
+
+    ls /tmp/ringgem-master >/dev/null
+
+  path: /root/install_nix.sh
+  append: true
+  permissions: "0755"
+
+
+- content: |
+    #!/usr/bin/env bash
+
+    set -e
+    set -u
+
+    curl -Lo ringgem.zip https://github.com/taylormonacelli/ringgem/archive/refs/heads/master.zip
+    unzip ringgem.zip
+
+  path: /root/install_ringgem.sh
+  append: true
+  permissions: "0755"
+
+- content: |
+    #!/usr/bin/env bash
+
+    set -e
+    set -u
+
+    if ! command task --version &>/dev/null; then
+        cd /usr/local
+        sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
+        /usr/local/bin/task --version
+    fi
+
+  path: /root/install_task.sh
+  append: true
+  permissions: "0755"
+
+runcmd:
+- /root/install_task.sh
+- /root/install_ringgem.sh
+#- /root/install_nix.sh
+EOF
+
+incus ls --format=json | jq 'map(select(.name == "{{ .Container }}")) | .[] | .name' | xargs --no-run-if-empty -I {} incus delete --force {}
+incus launch images:ubuntu/22.04/cloud {{ .Container }} --config=user.user-data="$(cat {{ .Container }}.yml)"
+incus exec {{ .Container }} -- cloud-init status --wait
+incus exec {{ .Container }} -- shutdown now
+
+# create nix image
+timeout 30s bash -c 'until incus publish {{ .Container }} --alias nix-jammy; do sleep 1s; done'
+
+incus ls --format=json | jq 'map(select(.name == "{{ .Container }}")) | .[] | .name' | xargs --no-run-if-empty -I {} incus delete --force {}
+
+# ensure nix-jammy image exists now:
+incus image list --format=json | jq -e 'map(select(.aliases[].name == "nix-jammy")) | length > 0' >/dev/null
+
+# launch new image from image we just created
+incus launch nix-jammy {{ .Container }}
+incus exec {{ .Container }} -- bash -c 'time nix --extra-experimental-features "nix-command flakes" search nixpkgs firefox'
